3f800db5f6d99efb731182ca28b257b05b97e2fa
Niklas Rönnholm <nikke.ronnholm@gmail.com>
2021-04-06 13:04:25 +0300

    One more protected route

--- resolvers/categoryResolver.js
+++ resolvers/categoryResolver.js
@@ -1,3 +1,4 @@
+import { AuthenticationError } from 'apollo-server-errors';
 import Category from '../models/categoryModel.js';
 
 export default {
@@ -5,7 +6,11 @@ export default {
     category: (parent) => Category.findById(parent.category)
   },
   Mutation: {
-    addCategory: (_, args) => {
+    addCategory: (_, args, context) => {
+      const { user } = context;
+
+      if (!user) throw new AuthenticationError('Not authenticated!');
+
       const newCategory = new Category(args);
       return newCategory.save();
     }
